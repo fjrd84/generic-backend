3b30435678f0d2b73f1a952351cd8eaf6a7c8ecc
F. Javier R. Donado <fjrd84@gmail.com>
2017-02-02 22:41:55 +0100

    JWT authentication example.

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,41 @@
 const express = require('express'),
   router = express.Router(),
+  jwt = require('jsonwebtoken'),
   User = require('../models/user');
+
+/**
+ * Middleware for the JSON web token validation.
+ */
+let tokenMiddleware = function (req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, 'secretString', function (err, decoded) {
+      if (err) {
+        return res.status(401).json({ message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+};
 
 /**
  * The auth router function receives a configured passport reference
@@ -37,25 +72,40 @@ module.exports = (passport) => {
     passport.authenticate('local-login',
       (err, user, info) => {
         if (!user) {
-          res.status(400).json({ message: "User not found" });
+          res.status(400).json({ message: info });
           return;
         }
-        console.log(user);
-        user.token = "asdfasdf";
-        res.json({
-          id: user.id,
-          token: user.token
+
+        /**
+         *  If user is found and password is right, create a token
+         */
+        var token = jwt.sign(user, 'secretString', {
+          expiresIn: "24h"
         });
+
+        user.token = token;
+        user.save((err) => {
+          if (err) res.send(err);
+          /**
+            * Return the information including token as JSON
+            */
+          res.json({
+            success: true,
+            id: user.id,
+            token: token
+          });
+        });
+
       })(req, res, next);
+  });
+
+  router.get('/tokentest', tokenMiddleware, function (req, res) {
+    res.json({ message: "Your token is valid! This endpoint has been authenticated." });
   });
   /*  router.post('/login', passport.authenticate('local-login', {
    }));*/
 
   // SIGNUP =================================
-  // show the signup form
-  /* router.get('/signup', function (req, res) {
-   res.render('signup.ejs', { message: req.flash('loginMessage') });
-  });*/
 
   // process the signup form
   router.post('/signup', (req, res, next) => {
@@ -65,7 +115,7 @@ module.exports = (passport) => {
           res.status(400).json({ message: info });
           return;
         }
-        console.log(user);
+        // TODO: Token from login...
         user.token = "asdfasdf";
         res.json({
           id: user.id,
