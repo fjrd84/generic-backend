d80f6ea11f386df5b5e9dbe47795f0cb23947718
F. Javier R. Donado <fjrd84@gmail.com>
2017-02-05 20:53:01 +0100

    Auth spec: add chai-http.

--- app/routes/auth-spec.js
+++ app/routes/auth-spec.js
@@ -1,13 +1,38 @@
-const request = require('supertest'),
+const chai = require('chai'),
+  expect = chai.expect,
+  chaiHttp = require('chai-http'),
   app = require('../../server'),
+  User = require('../models/user'),
   mongoose = require('mongoose');
+
+chai.use(chaiHttp);
 
 describe('Requests to the auth path', function () {
 
-  it('Returns Unauthorized when accessing protected routes',
+  it('GET /auth/profile -> Unauthorized',
     function (done) {
-      request(app)
+      chai.request(app)
         .get('/auth/profile')
-        .expect(401, done);
+        .end((err, res) => {
+          
+          //expect(err.text).to.equal("Unauthorized");
+          expect(res).to.have.status(401);
+          done();
+        });
     });
+
+  it('POST /auth/login -> Unauthorized', function (done) {
+    done();
+    /*    chai.request(app)
+          .post('/auth/login', {
+            email: "fake@fake.com",
+            password: "nopass"
+          })
+          .expect(400)
+          .end((err, res) => {
+            console.log(res.body);
+            assert(body.message);
+            done();
+          });*/
+  });
 });
