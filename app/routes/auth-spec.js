1eb8a3fcf737886f28886bb9e12770e3674c3ac3
F. Javier R. Donado <fjrd84@gmail.com>
2017-02-06 00:38:50 +0100

    Auth spec: user sign up.

--- app/routes/auth-spec.js
+++ app/routes/auth-spec.js
@@ -3,13 +3,31 @@ const chai = require('chai'),
   chaiHttp = require('chai-http'),
   app = require('../../server'),
   User = require('../models/user'),
+  environment = require('../../config/environment'),
   mongoose = require('mongoose');
 
 chai.use(chaiHttp);
 
 describe('Requests to the auth path', function () {
 
-  it('GET /auth/profile -> Unauthorized',
+  beforeEach(function (done) {
+    if (!mongoose.connection.db) {
+      mongoose.connect(environment.db, done);
+    }
+    // Make sure that there are no users in the collection before starting testing
+    User.remove({}, function (err) {
+      done();
+    });
+  });
+
+  afterEach((done) => {
+    // Clean the users collection after each test.
+    User.remove({}, function (err) {
+      done();
+    });
+  });
+
+  it('GET /auth/profile without token: Unauthorized',
     function (done) {
       chai.request(app)
         .get('/auth/profile')
@@ -20,14 +38,24 @@ describe('Requests to the auth path', function () {
         });
     });
 
-  it('POST /auth/login -> Unauthorized', function (done) {
+  it('POST /auth/login with fake data: Unauthorized', function (done) {
     chai.request(app)
       .post('/auth/login')
-      .field("email", "fake@fake.com")
-      .field("password", "nopass")
+      .send({ email: "fake@fake.com", password: "fakepass" })
       .end((err, res) => {
-        expect(res.body.message.message).to.equal("Missing credentials");
+        expect(res.body.message).to.equal("No user has been found.");
         done();
       });
   });
+
+  it('POST /auth/signup creates a new user', (done) => {
+    chai.request(app)
+      .post('/auth/signup')
+      .send({ email: "testuser@testdomain.com", password: "s3cr3t" })
+      .end((err, res) => {
+        expect(res.body.token).to.exist;
+        done();
+      });
+  });
+
 });
