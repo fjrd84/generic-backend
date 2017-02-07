2c88e99db5af3970e1d881adcbb01c29f26bdc4b
F. Javier R. Donado <fjrd84@gmail.com>
2017-02-07 22:51:49 +0100

    Passport callbacks specs: sign in and log in (local).

--- config/passport-callbacks-spec.js
+++ config/passport-callbacks-spec.js
@@ -1,8 +1,24 @@
 const chai = require('chai'),
   expect = chai.expect,
+  User = require('../app/models/user.js'),
   passportCallbacks = require('./passport-callbacks');
 
 describe('passport callbacks', () => {
+
+  let exampleUser = { local: { email: "mruser@company.com" } };
+
+  beforeEach('empty the users collection', (done) => {
+    User.remove({}, () => {
+      done();
+    });
+  });
+
+  after('empty the users collection', (done) => {
+    User.remove({}, () => {
+      done();
+    });
+  });
+
   it('should contain a callback for each strategy', (done) => {
     expect(passportCallbacks.localLogin).to.exist;
     expect(passportCallbacks.localSignup).to.exist;
@@ -20,11 +36,42 @@ describe('passport callbacks', () => {
     });
   });
 
-  it('should validate the user password using a local strategy', (done) => {
-    passportCallbacks.localLogin({}, null, "passw0rd", (err, user) => {
-      //todo
-      done();
+  it('should validate the user password using a local strategy',
+    (done) => {
+      let newUser = new User(exampleUser);
+      newUser.local.password = newUser.generateHash("passw0rd");
+      newUser.save(() => {
+        passportCallbacks.localLogin({}, exampleUser.local.email, "passw0rd", (err, user) => {
+          let userObject = user.toObject();
+          expect(userObject).to.have.a.property('_id').that.is.an('object');
+          done();
+        });
+      });
     });
-  });
 
+  it('should reject a wrong password using a local strategy',
+    (done) => {
+      let newUser = new User(exampleUser);
+      newUser.local.password = newUser.generateHash("passw0rd");
+      newUser.save(() => {
+        passportCallbacks.localLogin({}, exampleUser.local.email, "wrongPassw0rd", (err, user) => {
+          expect(user).to.be.false;
+          done();
+        });
+      });
+    });
+
+  it('should sign in a new user',
+    (done) => {
+      let email = "mrproper@limpio.com",
+          password = "cl3aning4ll";
+
+      passportCallbacks.localSignup({}, email, password, (err, user) => {
+        expect(user).to.be.an('object');
+        expect(user._doc.local.email).to.equal(email);
+        // The password must be hashed
+        expect(user._doc.local.password).to.not.equal(password);
+        done();
+      });
+    });
 });
